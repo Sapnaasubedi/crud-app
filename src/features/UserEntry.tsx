fc7ab8d74ef1f3c2de7f01f22b697175855dac69
sapana <sapanasubedi@takeo.ai>
2024-05-28 17:38:02 +0545

    feat: final fe setup

--- src/features/UserEntry.tsx
+++ src/features/UserEntry.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { Grid, Input, Stack } from '@mui/material';
 import React from 'react';
--- src/features/user.type.tsx
+++ src/features/user.type.tsx
@@ -2,5 +2,11 @@ export interface User {
     fname: string;
     lname:string;
     email:string;
+  }
+
+  export interface IUserEntry {
+    fname: string;
+    lname:string;
+    email:string;
     password:string;
   }
