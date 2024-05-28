c4548327a15642cf4a0649037cc5bb6e0cdc9a73
sapana <sapanasubedi@takeo.ai>
2024-05-28 11:53:11 +0545

    feat: fe setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'd3bmhjvpbebp8p.cloudfront.net',
+      },
+      {
+        protocol: 'http',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
+  redirects: async () => {
+    return [
+      {
+        source: '/full-stack-developer-bootcamp',
+        destination: '/software-engineering-bootcamp',
+        permanent: true,
+      },
+      {
+        source: '/bootcamp-syllabus/FullStack-Developer-Bootcamp-Syllabus.pdf',
+        destination:
+          '/bootcamp-syllabus/Software-Engineering-Bootcamp-Syllabus.pdf',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
